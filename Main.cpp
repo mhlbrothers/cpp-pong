8ce7640bc4741d0e374aabdefbcb0f5f0e78fbfd
WrongHanded <mhlbrothers@yahoo.com>
2019-03-25 21:30:05 -0500

    Got a basic OpenGL setup
    Note: I did not add error handling for simplicity.

--- Main.cpp
+++ Main.cpp
@@ -1,55 +1,98 @@
-//Using SDL and standard IO
 #include <SDL.h>
+#include <SDL_opengl.h>
+#include <GL\GLU.h>
 #include <stdio.h>
+#include <string>
 
-//Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main( int argc, char* args[] )
-{
-    //The window we'll be rendering to
-    SDL_Window* window = NULL;
-    
-    //The surface contained by the window
-    SDL_Surface* screenSurface = NULL;
+SDL_Window* Window = NULL;
 
-    //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-    {
-        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+SDL_GLContext glContext;
+
+bool renderQuad = true;
+
+void init(){
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 1);
+
+    Window = SDL_CreateWindow("Pong", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+
+    glContext = SDL_GL_CreateContext(Window);
+
+    SDL_GL_SetSwapInterval(1);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glClearColor( 0.0f, 0.0f, 0.0f, 1.0f);  
+}
+
+void handleKeys( unsigned char key, int x, int y){
+    if( key == 'q') {
+        renderQuad = !renderQuad;
     }
+}
 
-    else
-    {
-        //Create window
-        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if( window == NULL )
-        {
-            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-        }
+void update(){
 
-         else
-        {
-            //Get window surface
-            screenSurface = SDL_GetWindowSurface( window );
+}
 
-            //Fill the surface white
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-            
-            //Update the surface
-            SDL_UpdateWindowSurface( window );
+void render(){
+    glClear(GL_COLOR_BUFFER_BIT);
 
-            //Wait two seconds
-            SDL_Delay( 2000 );
-        }
+    if(renderQuad){
+        glBegin( GL_QUADS );
+			glVertex2f( -0.5f, -0.5f );
+			glVertex2f( 0.5f, -0.5f );
+			glVertex2f( 0.5f, 0.5f );
+			glVertex2f( -0.5f, 0.5f );
+		glEnd();
     }
+}
 
-      //Destroy window
-    SDL_DestroyWindow( window );
+void close(){
+    SDL_DestroyWindow(Window);
+    Window = NULL;
 
-    //Quit SDL subsystems
     SDL_Quit();
+}
+
+int main(int argc, char* args[]){
+    init();
+
+    bool quit = false;
+
+    SDL_Event e;
+
+    SDL_StartTextInput();
+
+    while(!quit){
+
+        while(SDL_PollEvent(&e) != 0){
+            if(e.type == SDL_QUIT){
+                quit = true;
+            } else if(e.type == SDL_TEXTINPUT){
+                int x = 0, y = 0;
+                SDL_GetMouseState( &x, &y);
+                handleKeys(e.text.text[0], x, y);
+            }
+        }
+
+        render();
+
+        SDL_GL_SwapWindow(Window);
+    }
+
+    SDL_StopTextInput();
+
+    close();
 
     return 0;
 }
